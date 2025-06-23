5f47ed65f4ed1dadbd37d827f33e97cc44a00841
Gerardo-Ontiveros <gontiverosdavila01@gamail.com>
2025-06-22 20:59:23 -0600

    feat: implement collapsible sidebar menu with routing

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,7 +1,63 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { Link, Outlet } from "react-router";
+import { MENU_DATA } from "../constants/Menu";
+import type { MenuTypes } from "../types/MenuTypes";
+import { AlignLeftOutlined, CloseOutlined } from "@ant-design/icons";
 
 function Dashboard() {
-  return <div>Dashboard</div>;
+  const [menuItems, setMenuItems] = useState<MenuTypes[]>([]);
+  const [openMenu, setOpenMenu] = useState<boolean>(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setMenuItems(MENU_DATA);
+    }, 500);
+  }, []);
+
+  const toggleMenu = () => {
+    setOpenMenu(!openMenu);
+  };
+
+  return (
+    <div className="flex flex-row  h-screen w-screen">
+      <div
+        className={`flex flex-col p-6 shadow-md  items-center  bg-neutral-900 transition-all duration-300 ${
+          openMenu ? "w-64" : "w-20"
+        }`}
+      >
+        <div className="flex w-full justify-end">
+          <button
+            className="px-2 cursor-pointer text-blue-300 dark:text-cyan-300  hover:text-blue-700 dark:hover:text-cyan-500 transition-colors"
+            onClick={toggleMenu}
+          >
+            {openMenu ? <CloseOutlined /> : <AlignLeftOutlined />}
+          </button>
+        </div>
+        <nav className="mt-6 flex flex-col gap-2">
+          {openMenu
+            ? menuItems.map((items) => (
+                <Link
+                  to={items.path}
+                  className="text-lg text-blue-300 dark:text-cyan-300 hover:text-blue-500 dark:hover:text-cyan-500 transition-colors"
+                >
+                  {items.icon} {items.title}
+                </Link>
+              ))
+            : menuItems.map((items) => (
+                <Link
+                  to={items.path}
+                  className="text-lg text-blue-300 dark:text-cyan-300"
+                >
+                  {items.icon}
+                </Link>
+              ))}
+        </nav>
+      </div>
+      <div className="flex-1">
+        <Outlet />
+      </div>
+    </div>
+  );
 }
 
 export default Dashboard;
